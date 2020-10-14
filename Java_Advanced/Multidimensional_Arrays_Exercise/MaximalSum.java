723720e3e0e6a826cc6ba8fd827cf399a2fe87b4
Martin Bojidarov <mbojidarov@gmail.com>
2020-10-15 01:37:05 +0300

    Adding task

---
+++ Java_Advanced/Multidimensional_Arrays_Exercise/MaximalSum.java
@@ -0,0 +1,73 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MaximalSum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] input = scanner.nextLine().split("\\s+");
+        int rows = Integer.parseInt(input[0]);
+        int cols = Integer.parseInt(input[1]);
+
+        int [][] matrix = new int[rows][cols];
+        readMatrix(matrix, scanner);
+
+  //      int[][] outputMatrix = new int[3][3];
+
+        int startRow = 0;
+        int startCol = 0;
+        int maxSum = Integer.MIN_VALUE;
+
+        for (int r = 0; r < rows - 2; r++) {
+            for (int c = 0; c < cols - 2; c++) {
+                int currentSum = matrix[r][c] + matrix[r][c + 1] + matrix[r][c + 2]
+                        + matrix[r + 1][c] + matrix[r + 1][c + 1] + matrix[r + 1][c + 2]
+                        + matrix[r + 2][c] + matrix[r + 2][c + 1] + matrix[r + 2][c + 2];
+                if (currentSum > maxSum){
+                    maxSum = currentSum;
+
+                    startRow = r;
+                    startCol = c;
+
+//                    outputMatrix[0][0] = matrix[r][c];
+//                    outputMatrix[0][1] = matrix[r][c + 1];
+//                    outputMatrix[0][2] = matrix[r][c + 2];
+//
+//                    outputMatrix[1][0] = matrix[r + 1][c];
+//                    outputMatrix[1][1] = matrix[r + 1][c + 1];
+//                    outputMatrix[1][2] = matrix[r + 1][c + 2];
+//
+//                    outputMatrix[2][0] = matrix[r + 2][c];
+//                    outputMatrix[2][1] = matrix[r + 2][c + 1];
+//                    outputMatrix[2][2] = matrix[r + 2][c + 2];
+                }
+            }
+        }
+
+        System.out.println(String.format("Sum = %d", maxSum));
+
+        for (int r = startRow; r <= startRow + 2; r++) {
+            for (int c = startCol; c <= startCol + 2; c++) {
+                System.out.print(matrix[r][c] + " ");
+            }
+            System.out.println();
+        }
+
+//        for (int r = 0; r < 3; r++) {
+//            for (int c = 0; c < 3; c++) {
+//                System.out.print(outputMatrix[r][c] + " ");
+//            }
+//            System.out.println();
+//        }
+    }
+    
+
+    private static int[][] readMatrix(int[][] matrix, Scanner scanner) {
+
+        for (int r = 0; r < matrix.length; r++) {
+            matrix[r] = Arrays.stream(scanner.nextLine().split("\\s+"))
+                    .mapToInt(Integer::parseInt).toArray();
+        }
+        return matrix;
+    }
+}
