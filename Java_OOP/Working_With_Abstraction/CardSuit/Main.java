d05447e2458ad100e897d942524a4a1476aff258
Martin Bojidarov <mbojidarov@gmail.com>
2020-11-06 08:54:52 +0200

    Modifying file

--- Java_OOP/Working_With_Abstraction/CardSuit/Main.java
+++ Java_OOP/Working_With_Abstraction/CardSuit/Main.java
@@ -7,8 +7,8 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Card Suits:");
-        for (CardSuit Enums : CardSuit.values()) {
-            System.out.printf("Ordinal value: %d; Name value: %s%n", Enums.ordinal(), Enums);
+        for (CardSuit enums : CardSuit.values()) {
+            System.out.printf("Ordinal value: %d; Name value: %s%n", enums.ordinal(), enums);
 
         }
     }
