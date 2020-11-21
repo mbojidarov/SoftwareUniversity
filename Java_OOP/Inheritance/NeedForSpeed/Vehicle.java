e5357d1f76835956dce46a5cadd0dd890f81a663
Martin Bojidarov <mbojidarov@gmail.com>
2020-11-22 01:06:03 +0200

    Adding files

---
+++ Java_OOP/Inheritance/NeedForSpeed/Vehicle.java
@@ -0,0 +1,50 @@
+package NeedForSpeed;
+
+public class Vehicle {
+
+   private final static double DEFAULT_FUEL_CONSUMPTION = 1.25;
+   private double fuelConsumption;
+   private double fuel;
+   private int horsePower;
+
+   public Vehicle(double fuel, int horsePower){
+       this.fuel = fuel;
+       this.horsePower = horsePower;
+       this.fuelConsumption = DEFAULT_FUEL_CONSUMPTION;
+   }
+
+    public void drive(double kilometers){
+       double currentDriveConsumption = (kilometers * this.fuelConsumption);
+       if (this.fuel - currentDriveConsumption >= 0){
+           this.setFuel(this.fuel -= currentDriveConsumption);
+       }
+    }
+
+    public double getFuelConsumption() {
+        return this.fuelConsumption;
+    }
+
+    public void setFuelConsumption(double fuelConsumption) {
+        this.fuelConsumption = fuelConsumption;
+    }
+
+    public double getFuel() {
+        return this.fuel;
+    }
+
+    public void setFuel(double fuel) {
+        this.fuel = fuel;
+    }
+
+    public int getHorsePower() {
+        return this.horsePower;
+    }
+
+    public void setHorsePower(int horsePower) {
+        this.horsePower = horsePower;
+    }
+
+    public static double getDefaultFuelConsumption() {
+        return DEFAULT_FUEL_CONSUMPTION;
+    }
+}
---
+++ Java_OOP/Inheritance/Zoo/Main.java
@@ -0,0 +1,10 @@
+package Zoo;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+    }
+}
